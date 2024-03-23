eededde89cfc157c71122af96f3e938d8c970e79
anshg03 <99557054+anshg03@users.noreply.github.com>
2024-03-23 16:44:20 +0530

    Updated Course Creation and Selecting course

--- nextjs/pages/api/Admin/createCourse.js
+++ nextjs/pages/api/Admin/createCourse.js
@@ -1,23 +1,42 @@
-import subject from "../../../models/subject"
-import connectDB from "../../../Middlewares/mongoose"
-
+import subject from "../../../models/subject";
+import connectDB from "../../../Middlewares/mongoose";
+import student from "../../../models/student";
 const handler = async (req, res) => {
-    try {
-        const {subjectCode, subjectName, instructorName, credits, department, totalLectures} = req.body
-        await subject.save({
-            subjectCode: subjectCode,
-            subjectName: subjectName,
-            department: department,
-            credits: credits,
-            year: year,
-            instructorName: instructorName,
-            totalLectures: totalLectures
-        })
-        res.json({message: "Subject created successfully..."}).status(200)
-    } catch (error) {
-        console.log(error)
-        res.json(error).status(500)
+  try {
+    const {
+      subjectCode,
+      subjectName,
+      instructorName,
+      credits,
+      department,
+      totalLectures,
+    } = req.body;
+    const newSubject = await new subject({
+      subjectCode: subjectCode,
+      subjectName: subjectName,
+      department: department,
+      credits: credits,
+      year: year,
+      instructorName: instructorName,
+      totalLectures: totalLectures,
+    });
+    await newSubject.save();
+    const students = await student.find({ department, year });
+    if (students.length !== 0) {
+      for (var i = 0; i < students.length; i++) {
+        students[i].subjects.push(newSubject._id);
+        await students[i].save();
+      }
     }
-}
+    return res.status(200).json({
+      success: true,
+      message: "Subject added successfully",
+      response: newSubject,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json(error).status(500);
+  }
+};
 
-export default connectDB(handler)
+export default connectDB(handler);
